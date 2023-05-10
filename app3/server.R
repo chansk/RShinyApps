cc7b807e13bfa2167a38256a08f0e489420afa99
Al <alexander.chansky@gmail.com>
2023-05-10 15:54:32 -0400

    add third elements

--- app3/server.R
+++ app3/server.R
@@ -15,6 +15,14 @@ function(input, output) {
            "cars" = cars)
   })
   
+  # Create caption
+    # renders input$caption as text when textInput caption field changes
+      # data-oriented reactive expressions do not depend on text,
+      # so figures and table expressions are NOT called when text changes
+  output$caption <- renderText({
+    input$caption
+  })
+  
   # Generate a summary of the dataset
   output$summary <- renderPrint({
     dataset <- datasetInput()
--- app3/ui.R
+++ app3/ui.R
@@ -3,8 +3,48 @@
 
 # Define UI (HTML webpage) that draws a histogram
   # fluidPage auto adjusts to browser dimensions
-fluidPage(
+# Define UI for dataset viewer app ----
+ui <- fluidPage(
   
+  # App title ----
+  titlePanel("Reactivity"),
+  
+  # Sidebar layout with a input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: text for providing caption
+        # Changes made to the caption in textInput control are 
+        # updated in output area while typing
+      textInput(inputId = "caption",
+                label = "Caption:",
+                value = "Data Summary"),
+      
+      # Input: Selector for choosing dataset ----
+      selectInput(inputId = "dataset",
+                  label = "Choose a dataset:",
+                  choices = c("rock", "pressure", "cars")),
+      
+      # Input: Numeric entry for number of obs to view ----
+      numericInput(inputId = "obs",
+                   label = "Number of observations to view:",
+                   value = 10)
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: formatted text for caption
+      h3(textOutput("caption", container = span)),
+      
+      # Output: Verbatim text for data summary ----
+      verbatimTextOutput("summary"),
+      
+      # Output: HTML table with requested number of observations ----
+      tableOutput("view")
+      
+    )
+  )
 )
-
-# 
